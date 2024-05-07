1a25ded551a2b2ad82800c08cf485a8e9af77019
yangli <577647772@qq.com>
2024-05-07 18:09:41 +0800

    auto_commit

--- Makefile
+++ Makefile
@@ -20,8 +20,10 @@ commit:
 	git add -f ./ModuleIntro/*.svg
 	git add -f ./UvmVeriTemplate/agents/**/*.sv
 	git add -f ./UvmVeriTemplate/uvm_tb/**/*.sv
-#git add -f ./UvmVeriTemplate/sim/filelist/*.f
-#git add -f ./UvmVeriTemplate/sim/Makefile
+	git add -f ./UvmVeriTemplate/sim/filelist/*.f
+	git add -f ./UvmVeriTemplate/sim/Makefile
+	git add -f ./UvmVeriTemplate/sim/novas.rc 
+	git add -f ./UvmVeriTemplate/sim/novas.conf
 	git commit -m "$(COMMIT_MSG)"
 	git push 
 
---
+++ UvmVeriTemplate/sim/filelist/uvm_tb.f
@@ -0,0 +1,27 @@
+// // ---------------------------------------------------------------------------------
+// //                 Copyright (c) 2022 
+// //                 ALL RIGHTS RESERVED
+// // ---------------------------------------------------------------------------------
+// // Filename      : uvm_tb.f
+// // Author        : AiF
+// // Created On    : 2022-05-15 21:12
+// // Last Modified : 
+// // ---------------------------------------------------------------------------------
+// // Description   : 
+// //
+// //
+// // ---------------------------------------------------------------------------------
+
+// +incdir+../uvm_tb/env
+// +incdir+../uvm_tb/sequences
+// +incdir+../uvm_tb/virtual_sequences
+// +incdir+../uvm_tb/tests
+
+// ../uvm_tb/env/my_env.sv
+// //../uvm_tb/sequences/asyn_fifo_seq_pkg.sv
+
+// //../uvm_tb/virtual_sequences/asyn_fifo_vseq_pkg.sv
+
+
+// ../uvm_tb/tests/base_test.sv
+// ../uvm_tb/**/*.sv
